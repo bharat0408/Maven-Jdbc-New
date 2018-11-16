ac7388f8bbb3d17814b7dcc9c7957b9f5b50cfec
bharat0408 <bm0408.bm@gmail.com>
2018-11-16 17:01:32 +0530

    Jdbc Maven new

---
+++ src/main/java/com/controller/InsertServlet.java
@@ -0,0 +1,57 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.Jdbc;
+import com.model.Person;
+
+/**
+ * Servlet implementation class InsertServlet
+ */
+public class InsertServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InsertServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String firstName = request.getParameter("fname");
+		String lastName = request.getParameter("lname");
+		PrintWriter out = response.getWriter();
+		Person p = new Person();
+		p.setFirstName(firstName);
+		p.setLastName(lastName);
+		
+		Jdbc x = new Jdbc();
+		int i  =x.saveData(p);
+		if(i>0) {
+			out.println("Bye");
+		//response.sendRedirect("index.jsp");
+		
+		}
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+	<form action="InsertServlet" method="post">
+		First Name <input type="text" name="fname"> 
+		Last Name  <input type="text" name="lname"> <input
+			type="submit" name="Click to Submit">
+	</form>
+</body>
+</html>
